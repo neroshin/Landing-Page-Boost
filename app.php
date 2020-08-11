76020e6da451b39fa9a3a90f288486bb02221515
Romano Ednalan <re4227@gmail.com>
2020-08-11 10:21:44 +0800

    New Plugin

---
+++ app.php
@@ -0,0 +1,50 @@
+<?php
+/**
+ * Plugin Name:			Landing Page Booster
+ * Plugin URI:			
+ * Description:	This performs dynamic keyword insertion into pages. It functions as a tool that aligns your keywords or set of keywords to be dynamically inserted within the pages of your website or within your landing pages. For CPC campaigns like AdWords, it does a great job of boosting visitor relevance and increases the conversion rate.
+
+ * Author URI: 	Romano Ednalan
+
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit;
+
+function KR_PlUGIN_URL() {
+	return untrailingslashit( plugins_url( '/', __FILE__ ) );
+}
+function KR_PlUGIN_PATH() {
+	return untrailingslashit( plugin_dir_path( __FILE__ ) );
+}
+define( 'PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
+require KR_PlUGIN_PATH() . '/model/plugin-update-checker/plugin-update-checker.php';
+
+/*
+ * Plugin Updater.
+ * 
+ * @link  https://github.com/YahnisElsts/plugin-update-checker#github-integration
+ */
+
+
+/*
+ * Check Update
+ */
+
+ 
+
+// If using a private repository, specify the access token.
+
+
+if ( ! function_exists( 'get_plugins' ) ) {
+	require_once ABSPATH . 'wp-admin/includes/plugin.php';
+}
+
+
+if( file_exists ( KR_PlUGIN_PATH() . '/main.php' ) ) {
+	
+	include KR_PlUGIN_PATH() . '/main.php';
+}
+
+
+
+?>
